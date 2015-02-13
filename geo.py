93c67a314574790e3e1acc505a4dea85aa316717
Richard Allen <rsaxvc@gmail.com>
2015-02-12 23:47:11 -0600

    Remove OpMap Support
    This repo will only support python-uls bindings

--- geo.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import GoogleV3
-geolocator = GoogleV3()
-location = geolocator.geocode("5022 LANSDOWNE AVE SAINT LOUIS MO 63109")
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw)
