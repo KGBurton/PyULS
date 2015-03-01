7f7bbd9a4e658bf5b9009c81057aebf8dfdf78b2
Richard Allen <rsaxvc@gmail.com>
2015-03-01 01:02:32 -0600

    Add a script to fetch some databases for testing.

---
+++ fetch.sh
@@ -0,0 +1,4 @@
+wget http://wireless.fcc.gov/uls/data/complete/l_frc.zip
+wget http://wireless.fcc.gov/uls/data/complete/l_coast.zip
+wget http://wireless.fcc.gov/uls/data/complete/l_ship.zip
+wget http://wireless.fcc.gov/uls/data/complete/l_aircr.zip
