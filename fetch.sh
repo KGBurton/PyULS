db1cd20b13b9f74d4344367d4274ef4d25c80b84
Richard <rsaxvc@rsaxvc.net>
2016-05-31 22:26:09 -0500

    fetch updates faster

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,5 @@
-wget http://wireless.fcc.gov/uls/data/complete/l_frc.zip
-wget http://wireless.fcc.gov/uls/data/complete/l_coast.zip
-wget http://wireless.fcc.gov/uls/data/complete/l_ship.zip
-wget http://wireless.fcc.gov/uls/data/complete/l_aircr.zip
+wget http://wireless.fcc.gov/uls/data/complete/l_frc.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_coast.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_ship.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_aircr.zip &
+wait
