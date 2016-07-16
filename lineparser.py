a541b485a6f2f472651de5d196878f87076ba3ec
Richard <rsaxvc@rsaxvc.net>
2016-07-16 09:56:53 -0500

    Fix a few more records with line breaks and such

--- lineparser.py
+++ lineparser.py
@@ -1,11 +1,20 @@
+#!/usr/bin/python
+#I am not proud of thise code. It mashes mysql CSV format into sqlite dump format.
+#However, I am 70% certain the FCC has a schizophrenic schema, as the number of columns sometimes changes.
 import sys
+import re
+
+def output(line):
+	sys.stdout.write(line.replace('"','\\"')+"\n")
+
 prev_line = ""
 for line in sys.stdin:
 	line = line.rstrip('\n')
-	if len(line) > 2 and line[2] == "|":
+	if re.search( "^[a-zA-Z0-9][a-zA-Z0-9]\|", line ):
 		if( prev_line != "" ):
-			sys.stdout.write(prev_line+"\n")
+			output(prev_line)
+			prev_line = ""
 		prev_line = line
 	else:
 		prev_line += line
-sys.stdout.write(prev_line+"\n")
+output(prev_line)
