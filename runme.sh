07b9e12700053bd6136cf8fef50e13b31260d8d8
Richard <rsaxvc@rsaxvc.net>
2016-07-16 12:19:39 -0500

    fix index generation

--- runme.sh
+++ runme.sh
@@ -6,5 +6,5 @@ rm "$DATABASE"
 ./create_schema.sh "$DATABASE" && \
 ./fetch_databases.sh && \
 ./populate_databases.py "$DATABASE" `find databases -name *.dat` && \
-./create_indexes.py "DATABASE" --index_file "$INDEXFILE" &&\
+./create_indexes.py "$DATABASE" --index_file "$INDEXFILE" &&\
 ./optimize.sh "$DATABASE"
