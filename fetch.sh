80d4cb20f67af61a8094dcecaf201503200a80eb
Richard <rsaxvc@rsaxvc.net>
2016-07-15 21:55:26 -0500

    Update DB fetcher with more DBs

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,11 @@
-wget http://wireless.fcc.gov/uls/data/complete/l_frc.zip &
-wget http://wireless.fcc.gov/uls/data/complete/l_coast.zip &
-wget http://wireless.fcc.gov/uls/data/complete/l_ship.zip &
-wget http://wireless.fcc.gov/uls/data/complete/l_aircr.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_aircr.zip   &
+wget http://wireless.fcc.gov/uls/data/complete/l_amat.zip    &
+wget http://wireless.fcc.gov/uls/data/complete/l_cell.zip    &
+wget http://wireless.fcc.gov/uls/data/complete/l_frc.zip     &
+wget http://wireless.fcc.gov/uls/data/complete/l_gmrs.zip    &
+wget http://wireless.fcc.gov/uls/data/complete/l_LMbcast.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_LMcomm.zip  &
+wget http://wireless.fcc.gov/uls/data/complete/l_LMpriv.zip  &
+wget http://wireless.fcc.gov/uls/data/complete/l_mdsitfs.zip &
+wget http://wireless.fcc.gov/uls/data/complete/l_paging.zip  &
 wait
